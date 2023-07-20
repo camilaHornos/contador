484277ffcdae5200e51287e3c32dce1a48af262a
Camila Hornos <hornoscami3@gmail.com>
2023-07-20 07:25:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let numero = 0;
+mas.onclick = () => {
+    numero++;
+    pantalla.value = numero;
+}
+
+
+let numero_menos = 0;
+menos.onclick = () => {
+    numero--;
+    pantalla.value = numero;
+}
